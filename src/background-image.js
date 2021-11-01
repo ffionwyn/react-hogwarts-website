96493701aa22c27da3937e6cb62e443c8e1ad071
ffionwyn <78923167+ffionwyn@users.noreply.github.com>
2021-11-01 19:36:40 +0000

    calling image function

--- src/background-image.js
+++ src/background-image.js
@@ -1,1 +1,10 @@
+import React from "react";
 import background from "./background.png";
+
+console.log(background);
+
+function Header() {
+  return <img src={background} alt="background" />;
+}
+
+export default Header;
