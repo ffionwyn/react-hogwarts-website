0b60057b00d851dece08d1361b8f039c4abdddc7
ffionwyn <78923167+ffionwyn@users.noreply.github.com>
2021-11-01 19:25:31 +0000

    Removed lines I don't need

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container"></div>
+      <div>
+        <footer>
+          <a
+            href="https://ffionwyn.github.io/react-hogwarts-website/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Open source code
+          </a>
+          <span> by Ffion Griffiths hosted on Netlify</span>
+        </footer>
+      </div>
     </div>
   );
 }
-
-export default App;
