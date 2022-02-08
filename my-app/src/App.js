c513288c81602df8c2db711022f9282562ab8617
ffionwyn <78923167+ffionwyn@users.noreply.github.com>
2022-02-08 15:23:38 +0000

    created headers
    created headers 1 & 3

---
+++ my-app/src/App.js
@@ -0,0 +1,26 @@
+export default function App() {
+  return (
+    <div className="App">
+      <div className="container"></div>
+      <div>
+        <h1>Welcome to Hogwarts!</h1>
+        <h3>
+          We are pleased to inform you that you have been accepted at Hogwarts
+          School of Witchcraft and Wizardry. <br />
+          Term begins on 1 September, we await your owl by no later than 31
+          July.
+        </h3>
+        <footer>
+          <a
+            href="https://ffionwyn.github.io/react-hogwarts-website/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Open source code
+          </a>
+          <span> by Ffion Griffiths hosted on Netlify</span>
+        </footer>
+      </div>
+    </div>
+  );
+}
